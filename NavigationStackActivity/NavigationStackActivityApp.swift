1885013e503b48e830b451bff903875ac5149b5b
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2025-04-03 16:44:24 +0530

    Initial Commit

---
+++ NavigationStackActivity/NavigationStackActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackActivityApp.swift
+//  NavigationStackActivity
+//
+//  Created by ARYAN SINGHAL on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationStackActivityTests/NavigationStackActivityTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackActivityTests.swift
+//  NavigationStackActivityTests
+//
+//  Created by ARYAN SINGHAL on 03/04/25.
+//
+
+import Testing
+@testable import NavigationStackActivity
+
+struct NavigationStackActivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
