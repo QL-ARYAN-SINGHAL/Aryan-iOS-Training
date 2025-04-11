0f8909a60e3fc7f4312e07fd62d39c5c969ff818
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2025-04-11 17:00:21 +0530

    Initial Commit

---
+++ Sports-Sync-AppTests/Sports_Sync_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_Sync_AppTests.swift
+//  Sports-Sync-AppTests
+//
+//  Created by ARYAN SINGHAL on 11/04/25.
+//
+
+import Testing
+@testable import Sports_Sync_App
+
+struct Sports_Sync_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
